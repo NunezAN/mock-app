9206d65425a9f1f3f5ae6eecb8a3d6e4ffbd4e53
NunezAN <alex101nunez@gmail.com>
2023-03-28 23:00:55 -0500

    test

--- src/components/PhonenumberInput.jsx
+++ src/components/PhonenumberInput.jsx
@@ -2,17 +2,7 @@ import React, { useState } from "react";
 
 const PhonenumberInput = () => {
   const [number, setNumber] = useState("");
-  const [value, setValue] = useState(initialValue);
-  const push = useCallback((item: T) => {
-    setValue(cur => [...cur, item])
-  }, [])
-
-  const removeByIndex = useCallback((index: number) => setValue(cur => {
-    const copy = [...cur];
-    copy.splice(index, 1);
-    return copy;
-  }), [])
-  return { value, push, removeByIndex };
+  //pomodoro
   // your code here
   return (
     <input
