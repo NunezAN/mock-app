177040093d1f038b520031db689ff2e225682de8
NunezAN <alex101nunez@gmail.com>
2023-04-03 09:17:03 -0500

    set counter as own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { useState } from "react";
-import Adding from "./components/Adding";
-import Subtract from "./components/Subtract";
-import CounterContext from "./context";
+import Counter from "./Counter";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const handleSubtraction = () => {
-    setCounter((cur) => (cur = cur - 1));
-  };
-  const handleAddition = () => {
-    setCounter((cur) => (cur = cur + 1));
-  };
   return (
-    <div className="bg-slate-400 p-8 text-2xl grid grid-cols-3 mt-8 mx-auto rounded-3xl max-w-xl">
-      <CounterContext.Provider
-        value={{ counter, handleAddition, handleSubtraction }}
-      >
-        <Subtract />
-        <div className="flex items-center justify-center p-4">{counter}</div>
-        <Adding />
-      </CounterContext.Provider>
+    <div>
+      <Counter></Counter>
     </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Adding from "./components/Adding";
+import Subtract from "./components/Subtract";
+import CounterContext from "./context";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const handleSubtraction = () => {
+    setCounter((cur) => (cur = cur - 1));
+  };
+  const handleAddition = () => {
+    setCounter((cur) => (cur = cur + 1));
+  };
+  return (
+    <div className="bg-slate-400 p-8 text-2xl grid grid-cols-3 mt-8 mx-auto rounded-3xl max-w-xl">
+      <CounterContext.Provider
+        value={{ counter, handleAddition, handleSubtraction }}
+      >
+        <Subtract />
+        <div className="flex items-center justify-center p-4">{counter}</div>
+        <Adding />
+      </CounterContext.Provider>
+    </div>
+  );
+}
+
+export default Counter;
